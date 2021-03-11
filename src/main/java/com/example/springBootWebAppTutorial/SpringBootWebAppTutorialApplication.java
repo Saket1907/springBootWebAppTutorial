09936b9ce44e586ff5ed3f808263de32a791b181
saku.isaket <saku.isaket@LAPTOP-S2GMU1FV>
2021-03-11 18:19:54 +0530

    ~ web app repo created  ~

---
+++ src/main/java/com/example/springBootWebAppTutorial/SpringBootWebAppTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootWebAppTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springBootWebAppTutorial/SpringBootWebAppTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootWebAppTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebAppTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
